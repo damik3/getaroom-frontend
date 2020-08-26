5104b1fcf891bc8801fe85b95c6d5e0271330bfe
damik3 <d1mike@hotmail.com>
2020-08-26 16:38:39 +0300

    another initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RoomComponent } from './room/room.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RoomComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatGridListModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/room/room.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RoomService } from '../services/room.service';
+import { PageEvent } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.css']
+})
+export class RoomComponent implements OnInit {
+
+  rooms = null;
+
+  numbers = null;
+
+  // MatPaginator inputs
+  length: number;
+  pageSize = 5;
+
+  // MatPaginator output
+  pageEvent: PageEvent;
+
+  constructor(private roomService: RoomService) { }
+
+  ngOnInit(): void {
+    this.pageEvent = new PageEvent();
+
+    this.numbers = Array(this.pageSize).fill(1).map((x,i)=>i); // [0,1,2,3,4]
+
+    this.roomService.getAll().subscribe(
+      data => {
+        this.rooms = data;
+        this.length = data.length;
+      },
+      err => {
+        this.rooms = JSON.parse(err.error).message;
+      }
+    );
+  }
+
+  getPageIndex() {
+    if (!this.pageEvent.pageIndex) {
+      return 0;
+    }
+    else {
+      return this.pageEvent.pageIndex;
+    }
+  }
+
+  getRoom(i: number) {
+      if (!this.length)
+        return null;
+
+      if ((i < 0) || (this.length <= i)) {
+          return null;
+      }
+      else {
+          return this.rooms[i];
+      }
+  }
+
+}
