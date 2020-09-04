56699fb638f223c936bca8b44a321bdc575537fa
damik3 <d1mike@hotmail.com>
2020-09-05 02:28:29 +0300

    Room: added title, country, city and area members

--- src/app/room-details/room-details.component.ts
+++ src/app/room-details/room-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomService } from '../services/room.service';
 import { ActivatedRoute } from '@angular/router';
+import { Room } from '../room';
 
 @Component({
   selector: 'app-room-details',
@@ -9,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RoomDetailsComponent implements OnInit {
 
-  room: any;
+  room: Room;
 
   constructor(private roomService: RoomService,
               private route: ActivatedRoute
--- src/app/room.ts
+++ src/app/room.ts
@@ -2,9 +2,13 @@ import { User } from './user';
 
 export class Room {
   id: string;
+  owner: User;
+  title: string;
+  country: string;
+  city: string;
+  area: string;
   address: string;
   description: string;
   numBeds: number;
   pricePerDay: number;
-  owner: User;
 }
