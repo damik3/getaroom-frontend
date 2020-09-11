6611391bf870133a343f128a51b16c5ab37f4fb7
damik3 <d1mike@hotmail.com>
2020-09-12 02:17:45 +0300

    Users' reservation impl

--- src/app/user-reservations/user-reservations.component.ts
+++ src/app/user-reservations/user-reservations.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../services/reservation.service';
+import { ActivatedRoute, Router} from '@angular/router';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Component({
   selector: 'app-user-reservations',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserReservationsComponent implements OnInit {
 
-  constructor() { }
+  reservations: any;
+  justBooked: string;
+
+  constructor(private reservationService: ReservationService,
+              private readonly router: Router,
+              private token: TokenStorageService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    // If user is not logged in, redirect to login
+    if (!this.token.getUser()) {
+      this.router.navigate(['/', 'login']);
+    }
+
+    this.route.queryParams.subscribe(params => {
+      this.justBooked = params.justBooked;
+    });
+
+    // Get user's reservations
+    this.reservationService.getUserReservations(this.token.getUser().id).subscribe(
+      data => {
+        this.reservations = data;
+      }
+    );
   }
 
 }
